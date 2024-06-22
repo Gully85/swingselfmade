9cf033aa6a6636ee2bdabd803b03a5b743caa2b4
Malte Lütje <Malte.Luetje@posteo.de>
2024-06-22 16:20:41 +0200

    reformulate game-tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,25 +1,42 @@
 # test around the game class
 
 import sys
+
 sys.path.append("S:/SwingSelfmade/")
 
 import game
+from balls import Ball
+from ongoing import FallingBall
 import unittest
 
+
 class TestTheGame(unittest.TestCase):
-    def test_game(self):
+
+    def test_game_init(self):
         game.reset()
 
-        # check initial values of score etc
         self.assertEqual(0, game.score)
         self.assertEqual(0, game.balls_dropped)
         self.assertEqual(4, game.level)
 
+    def test_game_levelup(self):
+        game.reset()
         # drop 52 balls. Check length of eventQueue. Level should have increased by then.
         for i in range(52):
             game.drop_ball()
-        self.assertEqual(52, game.ongoing.get_number_of_events())
-        self.assertIsInstance(game.ongoing.get_newest_event(), game.ongoing.FallingBall)
 
-if __name__ == '__main__':
+        self.assertEqual(5, game.level)
+
+    def test_dropped_ball_in_eventQueue(self):
+        game.reset()
+
+        the_ball: Ball = game.crane.getball()
+        game.drop_ball()
+
+        event: FallingBall = game.ongoing.get_newest_event()
+        self.assertIsInstance(event, FallingBall)
+        self.assertEqual(event.getball(), the_ball)
+
+
+if __name__ == "__main__":
     unittest.main()
