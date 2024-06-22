d74701f81d9a51544a3a462e631ca160a6c8c893
Malte Lütje <Malte.Luetje@posteo.de>
2024-06-22 13:08:23 +0200

    rework depot test

--- tests/test_depot.py
+++ tests/test_depot.py
@@ -2,26 +2,32 @@
 # is imported indirectly via import game, testing game.depot
 
 import sys
+
 sys.path.append("S:/SwingSelfmade/")
 
 import game, balls
-import unittest, random
+import unittest
+
 
 class TestTheDepot(unittest.TestCase):
-    def test_depot(self):
+
+    def test_depot_number_of_rows(self):
         game.reset()
 
         the_depot = game.depot
 
-        # drop a ball out of the 8 rows 0..7, verify type. Make sure out-of-bounds raises an IndexError
+        # Rows 0..7 must be able to drop a Ball
         for i in range(8):
             self.assertIsInstance(the_depot.next_ball(i), balls.Ball)
+
+        # Rows -1 and 8 must raise an IndexError
         with self.assertRaises(IndexError):
             the_depot.next_ball(-1)
         with self.assertRaises(IndexError):
             the_depot.next_ball(8)
-        
+
         # TODO once ball generation can force a certain ball, check if it comes out exactly two drops later
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
